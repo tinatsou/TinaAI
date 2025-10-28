e11fcec277325e80f9e705376745f734b2a05839
tinatsou <tinatsou6@gmail.com>
2025-10-27 19:17:36 -0700

    Delete edge_agent directory

--- edge_agent/device_interface.py
+++
@@ -1,9 +0,0 @@
-"""
-device_interface.py
-
-Stub for device interface definitions. This module should provide classes and functions to interact with various hardware devices.
-"""
-
-def read_sensor():
-    """Read data from a sensor (placeholder)."""
-    raise NotImplementedError("Sensor reading not implemented.")
